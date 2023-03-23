b4ba218760527da8a1bade3b87baa1d121c72349
KevinMi2023p <kevinmi2alter@gmail.com>
2023-03-23 18:58:37 -0400

    Removed commented out dead code

--- api_paths/src/main/java/com/spring_rest_api/api_paths/ApiPathsApplication.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/ApiPathsApplication.java
@@ -1,18 +1,5 @@
 package com.spring_rest_api.api_paths;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// @SpringBootApplication
-// public class ApiPathsApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(ApiPathsApplication.class, args);
-// 	}
-
-// }
-
-// package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
