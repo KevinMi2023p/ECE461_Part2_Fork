5149ea45db344037a0f440bb70ad7578877fb6b6
IshaanJain <jain343@purdue.edu>
2023-04-01 21:25:25 -0400

    Connecting FireStore

---
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/service/PackageService.java
@@ -0,0 +1,8 @@
+package com.spring_rest_api.api_paths.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PackageService {
+ // We will write code here that will be called in the contollers to send data to the db
+}
