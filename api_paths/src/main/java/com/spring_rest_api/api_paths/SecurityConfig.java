a87a018872e8c7f0d5233b47ff2fff2d4f37c189
Kahaan Patel <kahaan2912@gmail.com>
2023-04-08 21:56:18 -0400

    Implemented few changes according to security

--- api_paths/src/main/java/com/spring_rest_api/api_paths/SecurityConfig.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/SecurityConfig.java
@@ -1,28 +1,28 @@
 package com.spring_rest_api.api_paths;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .and()
-            .httpBasic();
+            .authorizeRequests(authorize -> authorize
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .permitAll()
+            );
     }
-
-    // @Override
-    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    // auth
-    //     .inMemoryAuthentication()
-    //     .withUser("user").password("{noop}password").roles("USER");
-    // }
-
 }
