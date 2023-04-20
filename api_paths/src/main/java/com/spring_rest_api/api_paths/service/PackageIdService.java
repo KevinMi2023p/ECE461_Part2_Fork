efd798cfcd9fbb12cc58d5829c8960d0ab44a777
KevinMi2023p <kevinmi2alter@gmail.com>
2023-04-19 23:59:42 -0400

    Added function to delete document

--- api_paths/src/main/java/com/spring_rest_api/api_paths/service/PackageIdService.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/service/PackageIdService.java
@@ -9,6 +9,7 @@ import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 import com.google.common.util.concurrent.ExecutionError;
 import com.google.firebase.cloud.FirestoreClient;
 
@@ -27,4 +28,17 @@ public class PackageIdService {
         return (document.exists()) ? document.getData().toString() : null;
     }
 
+
+    // Returns true if document found has been deleted from Packages collection
+    public boolean deletePackage(String id) throws ExecutionException, InterruptedException {
+        DocumentReference docRef = collectionReference.document(id);
+        DocumentSnapshot document = docRef.get().get();
+        if (!document.exists()) {return false;}
+        // Warning: Deleting a document does not delete its subcollections!
+        ApiFuture<WriteResult> writeRes = docRef.delete();
+
+        System.out.println("Update time : " + writeRes.get().getUpdateTime());
+
+        return true;
+    }
 }
