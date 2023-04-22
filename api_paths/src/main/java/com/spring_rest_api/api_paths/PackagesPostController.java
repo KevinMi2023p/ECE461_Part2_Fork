e7c4da615ac36b697d9af3c775ce95d8a9868c28
Jamie Laster <jamie.laster23@gmail.com>
2023-04-22 17:25:03 -0400

    Added a logger and constructor to the packages post controller

--- api_paths/src/main/java/com/spring_rest_api/api_paths/PackagesPostController.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/PackagesPostController.java
@@ -8,14 +8,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutionException;
 
 @RestController
 public class PackagesPostController {
 
+    private final Logger logger;
+
     @Autowired
-    PackageService packageService;
+    private PackageService packageService;
+
+    public PackagesPostController() {
+        this.logger = LoggerFactory.getLogger(this.getClass());
+    }
     
     @PostMapping("/packages")
     public void packages_plurual() {
