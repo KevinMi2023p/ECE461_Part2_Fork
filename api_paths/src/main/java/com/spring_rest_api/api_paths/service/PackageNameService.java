38e92db33bcb9c3054ff7f85ea9c2425c478da9a
KevinMi2023p <kevinmi2alter@gmail.com>
2023-04-24 01:07:37 -0400

    Added getByName service

--- api_paths/src/main/java/com/spring_rest_api/api_paths/service/PackageNameService.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/service/PackageNameService.java
@@ -1,11 +1,13 @@
 package com.spring_rest_api.api_paths.service;
 
 import java.util.concurrent.ExecutionException;
+import java.util.Map;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.core.ApiFuture;
-import com.google.api.services.storage.Storage.BucketAccessControls.List;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
@@ -13,6 +15,9 @@ import com.google.cloud.firestore.Query;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
+import com.google.gson.JsonArray;
+import com.google.gson.Gson;
+
 
 @Service
 public class PackageNameService {
@@ -30,6 +35,18 @@ public class PackageNameService {
         }
 
         return docsToRemove;
+    }
+
+    public String getByName(String _name) throws ExecutionException, InterruptedException {
+        Query query = collectionReference.whereEqualTo(fieldToCheck, _name);
+        ApiFuture<QuerySnapshot> future = query.get();
+
+        ArrayList<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+        for (DocumentSnapshot document : future.get().getDocuments()) {
+            result.add(document.getData());
+        }
+
+        return (!result.isEmpty()) ? new Gson().toJson(result) : null;
     }
 
 }
