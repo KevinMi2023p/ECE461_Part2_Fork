5d77dafe1242efa1cb49693fc0912b222e62a161
KevinMi2023p <kevinmi2alter@gmail.com>
2023-04-23 21:56:14 -0400

    DEL /package/byName/{name} endpoint

--- api_paths/src/main/java/com/spring_rest_api/api_paths/PackageByNameController.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/PackageByNameController.java
@@ -1,6 +1,14 @@
 package com.spring_rest_api.api_paths;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring_rest_api.api_paths.service.PackageNameService;
+
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,15 +16,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class PackageByNameController {
+    private final ResponseEntity<String> notFoundError = ResponseEntity.status(HttpStatus.NOT_FOUND).body("Package does not exist.");
+
+    @Autowired
+    PackageNameService packageNameService;
     
 	@GetMapping("/package/byName/{name}")
-	public String packageByName(@PathVariable String name) {
+	public String packageByName(@PathVariable String name) throws ExecutionException, InterruptedException {
+        // Integer res = packageNameService.removeByName(name);
+        // System.out.println(res);
+
 		return String.format("PackageByName %s!", name);
 	}
 
     @DeleteMapping("/package/byName/{name}")
-    public void deleteMethodName(@PathVariable String name) {
-        System.out.println("Delete!");
+    public ResponseEntity<String> deleteMethodName(@PathVariable String name) throws ExecutionException, InterruptedException {
+        Integer res = packageNameService.removeByName(name);
+        if (res == 0)
+            return notFoundError;
+        
+        String successMsg = "Package is deleted.";
+        return ResponseEntity.ok().body(successMsg);
     }
 
 }
