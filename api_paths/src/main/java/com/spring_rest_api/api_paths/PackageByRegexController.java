ff00676d26dd642dc9448f8f381142c229cb9799
KevinMi2023p <kevinmi2alter@gmail.com>
2023-04-25 17:56:25 -0400

    Regex Controller Calls Service Now

--- api_paths/src/main/java/com/spring_rest_api/api_paths/PackageByRegexController.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/PackageByRegexController.java
@@ -1,7 +1,14 @@
 package com.spring_rest_api.api_paths;
 
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring_rest_api.api_paths.entity.RegexSchema;
+import com.spring_rest_api.api_paths.service.RegexService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,11 +17,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class PackageByRegexController {
+
+    @Autowired
+    RegexService regexService;
     
     @PostMapping("/package/byRegEx")
-    public void postMethodName() {
+    public void postMethodName(@RequestBody RegexSchema regexSchema) throws ExecutionException, InterruptedException {
                 
-        System.out.println("Starting new api");
+        regexService.getVersionName(regexSchema);
 
         return;
     }
