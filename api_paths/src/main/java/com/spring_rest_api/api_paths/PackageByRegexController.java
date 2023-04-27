6729e8029ab87ccb0d281045dff9423a768fab75
KevinMi2023p <kevinmi2alter@gmail.com>
2023-04-27 01:12:30 -0400

    Integrated Package by Regex Controller

--- api_paths/src/main/java/com/spring_rest_api/api_paths/PackageByRegexController.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/PackageByRegexController.java
@@ -3,6 +3,7 @@ package com.spring_rest_api.api_paths;
 import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,16 +18,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class PackageByRegexController {
+    private final ResponseEntity<String> notFoundError = ResponseEntity.status(HttpStatus.NOT_FOUND).body("No package found under this regex.");
 
     @Autowired
     RegexService regexService;
     
     @PostMapping("/package/byRegEx")
-    public void postMethodName(@RequestBody RegexSchema regexSchema) throws ExecutionException, InterruptedException {
-                
-        regexService.getVersionName(regexSchema);
-
-        return;
+    public ResponseEntity<String> postMethodName(@RequestBody RegexSchema regexSchema) throws ExecutionException, InterruptedException {
+        String result = regexService.getVersionName(regexSchema);
+        if (result == null)
+            return notFoundError;
+        
+        return ResponseEntity.ok().body(result);
     }
     
 
