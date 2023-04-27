640326701d29dceb4aa8d500c7490217a07b349d
KevinMi2023p <kevinmi2alter@gmail.com>
2023-04-27 01:05:55 -0400

    RegexService finished

--- api_paths/src/main/java/com/spring_rest_api/api_paths/service/RegexService.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/service/RegexService.java
@@ -1,6 +1,7 @@
 package com.spring_rest_api.api_paths.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
@@ -14,6 +15,7 @@ import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.internal.NonNull;
+import com.google.gson.Gson;
 import com.spring_rest_api.api_paths.entity.RegexSchema;
 
 @Service
@@ -26,9 +28,11 @@ public class RegexService {
     private CollectionReference collectionReference = FirestoreClient.getFirestore().collection(COLLECTION_NAME);
 
 
-    public void getVersionName(RegexSchema regexSchema) throws ExecutionException, InterruptedException {
+    public String getVersionName(RegexSchema regexSchema) throws ExecutionException, InterruptedException {
         Pattern pattern = Pattern.compile(regexSchema.getRegex());
         
+        ArrayList<Map<String,String>> result = new ArrayList<Map<String,String>>();
+
         // https://firebase.google.com/docs/firestore/solutions/search?provider=algolia
         // Firestore doesn't have a query by Regex function, they suggested 3rd party solutions
         // I'm don't feel like doing that, this way works but slight performance hit (still fast)
@@ -36,20 +40,23 @@ public class RegexService {
 
         for (DocumentSnapshot document : querySnapshot.get().getDocuments()) {
             Map<String, Object> data = document.getData();
-            String name = (String) ((Map<String, Object>) data.get("metadata")).get("Name");    //ignore warning
+            Map<String, Object> metaData = (Map<String, Object>) data.get("metadata");
+            
+            String name = (String) metaData.get("Name");    //ignore warning
             Matcher match = pattern.matcher(name);
+            
             if (match.find()) {
-                System.out.println(name);
+                Map<String, String> data_point = new HashMap<String, String>();
+                data_point.put("Name", name);
+                String version = (String) metaData.get("Version");
+                data_point.put("Version", version);
+                result.add(data_point);
             }
             
         }
 
 
-
-        // ArrayList<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
-
-
-
+        return (result.isEmpty()) ? null : new Gson().toJson(result);
     }
 
 }
