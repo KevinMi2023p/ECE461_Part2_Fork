56b466f1df3c0d1cc6e75d71aabcbc1d7fd3699e
KevinMi2023p <kevinmi2alter@gmail.com>
2023-04-27 00:43:04 -0400

    Debugged RegexSchema

--- api_paths/src/main/java/com/spring_rest_api/api_paths/entity/RegexSchema.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/entity/RegexSchema.java
@@ -4,14 +4,17 @@ import com.google.cloud.firestore.annotation.PropertyName;
 
 public class RegexSchema {
     
-    private String regex;
+    public String RegEx;
 
     @PropertyName("RegEx")
     public void setRegex(String _reg) {
-        this.regex = _reg;
+        // this.regex = _reg;
+        RegEx = _reg;
     }
 
+    @PropertyName("RegEx")
     public String getRegex() {
-        return this.regex;
+        // return this.regex;
+        return RegEx;
     }
 }
