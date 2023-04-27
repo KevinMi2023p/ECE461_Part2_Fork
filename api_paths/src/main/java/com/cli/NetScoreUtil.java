24297a0ec7415a78b08c51038429665e6feb3906
Jamie Laster <jamie.laster23@gmail.com>
2023-04-27 11:34:46 -0400

    Converted the main method to a testing method

--- api_paths/src/main/java/com/cli/NetScoreUtil.java
+++ api_paths/src/main/java/com/cli/NetScoreUtil.java
@@ -5,11 +5,11 @@ public class NetScoreUtil {
     
     public static native NetScoreMetric CalculateNetScore(String url);
     
-    public static void main(String[] args) {
-        String github = "https://github.com/Alethon/ECE461_Part2";
-        NetScoreMetric nsm = CalculateNetScore(github);
-        System.out.println(nsm == null);
-        nsm = CalculateNetScore("athihgagg");
-        System.out.println(nsm == null);
+    public static NetScoreMetric[] test() {
+        // this github
+        NetScoreMetric nsm1 = CalculateNetScore("https://github.com/Alethon/ECE461_Part2");
+        // a string that should fail
+        NetScoreMetric nsm2 = CalculateNetScore("athihgagg");
+        return new NetScoreMetric[] { nsm1, nsm2 };
     }
 }
