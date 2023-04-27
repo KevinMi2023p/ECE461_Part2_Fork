62ffd75f5d1c70e45f3dc6d1baa1c79b86809755
Jamie Laster <jamie.laster23@gmail.com>
2023-04-27 11:05:32 -0400

    Moved the java classes that call the go code to the java workspace

--- api_paths/src/main/java/com/cli/NetScoreMetric.java
+++ api_paths/src/main/java/com/cli/NetScoreMetric.java
@@ -1,4 +1,4 @@
-package cli;
+package com.cli;
 
 public class NetScoreMetric {
     public float License;
--- api_paths/src/main/java/com/cli/NetScoreUtil.java
+++ api_paths/src/main/java/com/cli/NetScoreUtil.java
@@ -1,10 +1,10 @@
-package cli;
+package com.cli;
 
 public class NetScoreUtil {
-    // static { System.load("/usr/lib/x86_64-linux-gnu/NetScoreUtil.so"); }
     static { System.loadLibrary("NetScoreUtil"); }
+    
     public static native NetScoreMetric CalculateNetScore(String url);
-
+    
     public static void main(String[] args) {
         String github = "https://github.com/Alethon/ECE461_Part2";
         NetScoreMetric nsm = CalculateNetScore(github);
