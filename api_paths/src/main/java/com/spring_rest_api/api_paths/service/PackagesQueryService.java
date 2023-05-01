c3858e6d249235a0703bd3381d8f44e099db55f2
KevinMi2023p <kevinmi2alter@gmail.com>
2023-05-01 17:47:51 -0400

    Matches based on type of query

--- api_paths/src/main/java/com/spring_rest_api/api_paths/service/PackagesQueryService.java
+++ api_paths/src/main/java/com/spring_rest_api/api_paths/service/PackagesQueryService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
@@ -13,31 +15,39 @@ import com.google.cloud.firestore.Query;
 import com.google.firebase.cloud.FirestoreClient;
 import com.spring_rest_api.api_paths.entity.PagQuery;
 
+// import ch.qos.logback.core.boolex.Matcher;
+
 @Service
 public class PackagesQueryService {
     private final String COLLECTION_NAME = "Packages";
     private final String fieldToCheck = "metadata.Name";
     private CollectionReference collectionReference = FirestoreClient.getFirestore().collection(COLLECTION_NAME);
 
+    private int type_of_query(String version_query) {
+        String types[] = {"Exact", "Bounded range", "Carat", "Tilde"};
+        for (int i = 0; i < types.length; i++) {
+            Pattern pattern = Pattern.compile(types[i]);
+            Matcher match = pattern.matcher(version_query);
+            if (match.find()) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
     public String pagnitatedqueries(List<PagQuery> pagQuerys) throws ExecutionException, InterruptedException {
-        // String name_match = pagQuery.get_Name();
-        // // String queries[] = pagQuery.get_Version().split("\\r?\\n|\\r"); piazza says that it's just examples of what it could be, not multiple here
-
-        // There is no or query for java
-        // java for andriod doesn't seem to work unfortunatley
-        // Query query = collectionReference.where(Filter.or(
-        //     Filter.equalTo(fieldToCheck, name_match),
-        //     Filter.equalTo("metadata.Version", "1.2.3")
-        // ));
+        // Note, there is no OR query for Java on Firestore
 
         List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
 
-        for(PagQuery pq : pagQuerys) {
-            // System.out.println(pq.get_Name() + " " + pq.get_Version());
+        for(PagQuery pags : pagQuerys) {
+            Query query = collectionReference.whereEqualTo(fieldToCheck, pags.get_Name());
+            int request_type = this.type_of_query(pags.get_Version());
+            if (request_type == -1) {
+                // log invalid Query
+                continue;
+            }
             
-
-
         }
 
         return "";
