85dcb2ca0bec667b4cf95975dba17196afa84a1d
Kahaan Patel <kahaan2912@gmail.com>
2023-05-02 23:24:55 -0400

    Update docker.bash

--- docker.bash
+++ docker.bash
@@ -12,8 +12,16 @@ javac api_paths/src/main/java/com/spring_rest_api/cli/*.java -h ./cli
 
 g++ -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -shared -o /usr/lib/libNetScoreUtil.so cli/com_spring_rest_api_cli_NetScoreUtil.cpp /usr/lib/libpackageanalyze.so
 
-export GOOGLE_APPLICATION_CREDENTIALS=accountKey.json
-NAME=$GOOGLE_APPLICATION_CREDENTIALS printenv NAME
+GOOGLE_APPLICATION_CREDENTIALS=accountKey.json
+
+# Append the variable to the user's shell profile file
+echo "export GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS" >> ~/.bashrc
+
+# Source the profile file to update the environment
+source ~/.bashrc
+
+NAME=GOOGLE_APPLICATION_CREDENTIALS echo "$NAME"
+cat accountKey.json &&
 
 ls 
 ls api_paths/src/main/resources/githubToken.txt &&
